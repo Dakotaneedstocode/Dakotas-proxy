a8d64d54d6564018ebde71022d07a82a5d03fd05
Dakotaneedstocode <djantem1@stu.k12.wv.us>
2026-08-04 21:26:20 -0400

    Add Tabs Module with initialization logic

---
+++ js/tabs.js
@@ -0,0 +1,14 @@
+/*
+ * Dakota's Basement
+ * Tabs Module
+ */
+
+window.BasementTabs = {
+    init() {
+        console.log("Tabs module loaded.");
+    }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    BasementTabs.init();
+});
