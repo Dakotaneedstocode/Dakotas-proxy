346ae6507dccdfbcaf8cb8520bfb6ec689ba22e5
Dakotaneedstocode <djantem1@stu.k12.wv.us>
2026-08-11 15:49:28 -0400

    Update service worker import path

--- sw.js
+++ sw.js
@@ -1,7 +1,1 @@
-// Dakota's Basement - Scramjet controller service worker
-//
-// This is the root service-worker entry point.
-// The actual Scramjet controller implementation lives in:
-//   /controller/controller.sw.js
-
-importScripts("/controller/controller.sw.js");
+importScripts("/scramjet/sw.js");
